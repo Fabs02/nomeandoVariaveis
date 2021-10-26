c50bb35e80e286b7643263639a2813af51b8dacb
Fabricio Calvanese Augusto <fabriciocalvanese@gmail.com>
2021-10-26 10:26:44 -0300

    Adicionado o ano de nascimento

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ fun main(args: Array<String>) {
     var prefDeEmprego: String = ""
     var nacionalidade: String = ""
     var tipoSanguineo: String = ""
+    var ano = 2021
 
     print("Digite seu nome: ")
     nome = readLine()!!
@@ -16,7 +17,10 @@ fun main(args: Array<String>) {
     print("Digite o seu tipo sanguíneo: ")
     tipoSanguineo = readLine()!!
 
+    var anoNasc = ano - idade
+
     println("$nome tem $idade de idade, tem preferência no cargo de $prefDeEmprego," +
-    " sua nacionalidade é $nacionalidade e seu tipo sanguíneo é $tipoSanguineo")
+    " sua nacionalidade é $nacionalidade e seu tipo sanguíneo é $tipoSanguineo. " +
+            "Além de ter nascido no ano de $anoNasc")
 }
 
